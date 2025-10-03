298e3eadb206fead274c121dff37ab817a1cfe63
Renato Muller Reinhold <renatoreinhold2@gmail.com>
2025-10-02 21:33:06 -0300

    update frontend

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './features/login/login.component';
+import { ProductsComponent } from './features/products/products.component';
+import { DashboardComponent } from './features/dashboard/dashboard.component';
+import { NotFoundComponent } from './features/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'products', component: ProductsComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: '**', component: NotFoundComponent }
+];
